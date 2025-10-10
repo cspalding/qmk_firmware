ac3cc6e9065d85916520e36656068db42c3a3a45
Chris Spalding <cspalding93@gmail.com>
2025-10-10 14:16:45 -0700

    current map

--- keyboards/inland/mk47/keymaps/inland_default/keymap.c
+++ keyboards/inland/mk47/keymaps/inland_default/keymap.c
@@ -19,16 +19,21 @@
 enum custom_keycodes {
     LAYERRAISE = SAFE_RANGE,
     LAYERDROP,
+    WINCYC,
+    LN_STRT,
+    LN_END,
+    WRDLEFT,
+    WRDRGHT
 };
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [0] = LAYOUT_planck_mit(
-        KC_ESC,  KC_Q,       KC_W,       KC_E,    KC_R,       KC_T, KC_Y, KC_U,       KC_I,    KC_O,    KC_P,    KC_TAB,
-        KC_BSPC, LT(1,KC_A), LT(4,KC_S), KC_D,    LT(2,KC_F), KC_G, KC_H, LT(3,KC_J), KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Z,       KC_X,       KC_C,    KC_V,       KC_B, KC_N, KC_M,       KC_COMM, KC_DOT,  KC_SLSH, LSFT_T(KC_ENT),
-        KC_NO,   KC_LCTL,    KC_LALT,    KC_LCMD, LAYERDROP,    KC_SPC,   LAYERRAISE, KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT
+        KC_ESC,  KC_Q,       KC_W,       KC_E,    KC_R, KC_T, KC_Y,   KC_U, KC_I,     KC_O,    KC_P,    KC_TAB,
+        KC_BSPC, LT(1,KC_A), LT(2,KC_S), KC_D,    KC_F, KC_G, KC_H,   KC_J, KC_K,     KC_L,    KC_SCLN, KC_QUOT,
+        KC_LSFT, KC_Z,       KC_X,       KC_C,    KC_V, KC_B, KC_N,   KC_M, KC_COMM,  KC_DOT,  KC_SLSH, LSFT_T(KC_ENT),
+        KC_NO,   KC_LCTL,    KC_LALT,    KC_LCMD, MO(3),    KC_SPC,   MO(4), KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT
     ),
 
     [1] = LAYOUT_planck_mit(
@@ -39,24 +44,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [2] = LAYOUT_planck_mit(
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_LCTL, KC_LALT, KC_LCMD, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_RSFT,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, LN_STRT, LN_END,  WINCYC,  KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, WRDLEFT, WRDRGHT, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MSTP, KC_MUTE, KC_MPRV, KC_MPLY, KC_MNXT, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,      KC_TRNS, KC_TRNS, KC_VOLU, KC_VOLD, KC_TRNS
     ),
 
     [3] = LAYOUT_planck_mit(
-        KC_TRNS, KC_GRV,  KC_LPRN, KC_LBRC, KC_LCBR, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_COMM, KC_RPRN, KC_RBRC, KC_RCBR, KC_TRNS, KC_TRNS, KC_TRNS,  KC_RSFT, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_DOT,  KC_TILD, KC_PIPE, KC_UNDS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_RSFT,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,      KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, LN_STRT, KC_TRNS, KC_TRNS, LN_END, KC_TRNS,
+        KC_TRNS, KC_LCTL, KC_LALT, KC_LCMD, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, WRDLEFT, KC_TRNS, KC_TRNS, WRDRGHT, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
 
     [4] = LAYOUT_planck_mit(
-        KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7, KC_F8,  KC_F9, KC_F10,  RM_NEXT,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MSTP, KC_MUTE, KC_MPRV, KC_MPLY, KC_MNXT, RM_VALU, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,      KC_TRNS, KC_TRNS, KC_VOLU, KC_VOLD, KC_TRNS
+        KC_TRNS, KC_GRV,  KC_LPRN, KC_LBRC, KC_LCBR, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_COMM, KC_RPRN, KC_RBRC, KC_RCBR, KC_TRNS, KC_TRNS, KC_TRNS,  KC_RSFT, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_DOT,  KC_EQL,  KC_PIPE, KC_UNDS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_RSFT,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,      KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
 
 };
@@ -69,15 +74,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     switch(get_highest_layer(layer_state|default_layer_state)) {
         default:
-            rgb_matrix_set_color_all(RGB_WHITE);
+            rgb_matrix_set_color_all(0, 0, 0);
             break;
 
         case 0:
-            rgb_matrix_set_color_all(RGB_WHITE);
+            rgb_matrix_set_color_all(0, 0, 0);
             rgb_matrix_set_color(13, RGB_CHARTREUSE);
             rgb_matrix_set_color(14, RGB_CORAL);
-            rgb_matrix_set_color(16, RGB_PURPLE);
-            rgb_matrix_set_color(19, RGB_CYAN);
+            rgb_matrix_set_color(40, RGB_PURPLE);
+            rgb_matrix_set_color(42, RGB_CYAN);
             break;
 
         case 1:
@@ -104,22 +109,47 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
             rgb_matrix_set_color(8, RGB_CHARTREUSE); // 8
             rgb_matrix_set_color(9, RGB_CHARTREUSE); // 9
             break;
-
+        
         case 2:
             rgb_matrix_set_color_all(0, 0, 0);
+
+            rgb_matrix_set_color(7, RGB_CORAL);
+            rgb_matrix_set_color(8, RGB_CORAL);
+            rgb_matrix_set_color(9, RGB_CORAL);
+
+            rgb_matrix_set_color(19, RGB_CORAL);
+            rgb_matrix_set_color(20, RGB_CORAL);
+
+            rgb_matrix_set_color(29, RGB_CORAL);
+            rgb_matrix_set_color(30, RGB_CORAL);
+            rgb_matrix_set_color(31, RGB_CORAL);
+            rgb_matrix_set_color(32, RGB_CORAL);
+            rgb_matrix_set_color(33, RGB_CORAL);
+            rgb_matrix_set_color(44, RGB_CORAL);
+            rgb_matrix_set_color(45, RGB_CORAL);
+            break;
+
+        case 3:
+            rgb_matrix_set_color_all(0, 0, 0);
         
+            rgb_matrix_set_color(7, RGB_PURPLE);
+            rgb_matrix_set_color(10, RGB_PURPLE);
+
+            rgb_matrix_set_color(13, RGB_PURPLE);
+            rgb_matrix_set_color(14, RGB_PURPLE);
+            rgb_matrix_set_color(15, RGB_PURPLE);
+
             rgb_matrix_set_color(19, RGB_PURPLE);
             rgb_matrix_set_color(20, RGB_PURPLE);
             rgb_matrix_set_color(21, RGB_PURPLE);
             rgb_matrix_set_color(22, RGB_PURPLE);
 
-            rgb_matrix_set_color(13, RGB_PURPLE);
-            rgb_matrix_set_color(14, RGB_PURPLE);
-            rgb_matrix_set_color(15, RGB_PURPLE);
-            rgb_matrix_set_color(16, RGB_PURPLE);
+            rgb_matrix_set_color(31, RGB_PURPLE);
+            rgb_matrix_set_color(34, RGB_PURPLE);
+
             break;
 
-        case 3:
+        case 4:
             rgb_matrix_set_color_all(0, 0, 0);
 
             // a bunch of symbols
@@ -140,29 +170,6 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
             rgb_matrix_set_color(28, RGB_CYAN);
             break;
 
-        
-        case 4:
-            rgb_matrix_set_color_all(0, 0, 0);
-
-            rgb_matrix_set_color(1, RGB_CORAL);
-            rgb_matrix_set_color(2, RGB_CORAL);
-            rgb_matrix_set_color(3, RGB_CORAL);
-            rgb_matrix_set_color(4, RGB_CORAL);
-            rgb_matrix_set_color(5, RGB_CORAL);
-            rgb_matrix_set_color(6, RGB_CORAL);
-            rgb_matrix_set_color(7, RGB_CORAL);
-            rgb_matrix_set_color(8, RGB_CORAL);
-            rgb_matrix_set_color(9, RGB_CORAL);
-            rgb_matrix_set_color(10, RGB_CORAL);
-
-            rgb_matrix_set_color(29, RGB_CORAL);
-            rgb_matrix_set_color(30, RGB_CORAL);
-            rgb_matrix_set_color(31, RGB_CORAL);
-            rgb_matrix_set_color(32, RGB_CORAL);
-            rgb_matrix_set_color(33, RGB_CORAL);
-            rgb_matrix_set_color(44, RGB_CORAL);
-            rgb_matrix_set_color(45, RGB_CORAL);
-            break;
     }
     return false;
 }
@@ -190,19 +197,33 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             row, col, led, get_u16_str(keycode, 16)
         );
 
-        
-        if (keycode == LAYERRAISE) {
-            uint8_t layer = get_highest_layer(layer_state);
-            uint8_t next  = (layer + 1) % 5;
-            uprintf("moving up to layer %u from layer %u", next, get_highest_layer(layer_state));
-            layer_move(next);
-            return false;
-        } else if (keycode == LAYERDROP) {
-            uint8_t layer = get_highest_layer(layer_state);
-            uint8_t prev = layer == 0 ? 4 : layer - 1;
-            uprintf("moving down to layer %u from layer %u", prev, get_highest_layer(layer_state));
-            layer_move(prev);
-            return false;
+        switch (keycode) {
+            case WINCYC:
+                // macOS example: Cmd + `
+                register_code(KC_LCMD);
+                tap_code(KC_GRV);
+                unregister_code(KC_LCMD);
+                break;
+            case LN_STRT:
+                register_code(KC_LCMD);
+                tap_code(KC_LEFT);
+                unregister_code(KC_LCMD);
+                break;
+            case LN_END:
+                register_code(KC_LCMD);
+                tap_code(KC_RIGHT);
+                unregister_code(KC_LCMD);
+                break;
+            case WRDLEFT:
+                register_code(KC_LALT);
+                tap_code(KC_LEFT);
+                unregister_code(KC_LALT);
+                break;
+            case WRDRGHT:
+                register_code(KC_LALT);
+                tap_code(KC_RIGHT);
+                unregister_code(KC_LALT);
+                break;
         }
     }
     return true;
