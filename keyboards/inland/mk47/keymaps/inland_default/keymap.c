aed0505fa2576a0627ce1fb8882564ffdd1f5d71
Chris Spalding <chrissp@remitly.com>
2025-10-13 13:14:37 -0700

    shift over apostrophe not enter

--- keyboards/inland/mk47/keymaps/inland_default/keymap.c
+++ keyboards/inland/mk47/keymaps/inland_default/keymap.c
@@ -31,8 +31,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [0] = LAYOUT_planck_mit(
         KC_ESC,  KC_Q,       KC_W,       KC_E,    KC_R, KC_T, KC_Y,   KC_U, KC_I,     KC_O,    KC_P,    KC_TAB,
-        KC_BSPC, LT(1,KC_A), LT(2,KC_S), KC_D,    KC_F, KC_G, KC_H,   KC_J, KC_K,     KC_L,    KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Z,       KC_X,       KC_C,    KC_V, KC_B, KC_N,   KC_M, KC_COMM,  KC_DOT,  KC_SLSH, LSFT_T(KC_ENT),
+        KC_BSPC, LT(1,KC_A), LT(2,KC_S), KC_D,    KC_F, KC_G, KC_H,   KC_J, KC_K,     KC_L,    KC_SCLN, LSFT_T(KC_QUOT),
+        KC_LSFT, KC_Z,       KC_X,       KC_C,    KC_V, KC_B, KC_N,   KC_M, KC_COMM,  KC_DOT,  KC_SLSH, KC_ENT,
         KC_NO,   KC_LCTL,    KC_LALT,    KC_LCMD, MO(3),    KC_SPC,   MO(4), KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT
     ),
 
